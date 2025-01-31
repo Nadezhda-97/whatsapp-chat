4856612a7643f1f767c4fff98d759366296dbafd
nnadya <n.d.niki2012@gmail.com>
2025-02-01 00:56:23 +0300

    update App.jsx

--- whatsapp-chat/src/App.jsx
+++ whatsapp-chat/src/App.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './components/Login';
 import Chat from './components/Chat';
+
+const loginPath = '/login';
+const chatPath = '/';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [credentials, setCredentials] = useState({ idInstance: '', apiTokenInstance: '' });
 
+  const Root = ({ children }) => {
+    return !isLoggedIn ? children : <Navigate to={chatPath} />
+  }
+
   return (
-    <div className='app'>
-      {!isLoggedIn ? (
-        <Login setIsLoggedIn={setIsLoggedIn} setCredentials={setCredentials} />
-      ) : (
-          <Chat credentials={credentials} />
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={loginPath} element={<Root><Login setIsLoggedIn={setIsLoggedIn} setCredentials={setCredentials} /></Root>} />
+        <Route path={chatPath} element={<Chat credentials={credentials} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
