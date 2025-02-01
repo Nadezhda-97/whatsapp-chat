9c8292ffd5d7227f8eae86a6439ca38077d47423
nnadya <n.d.niki2012@gmail.com>
2025-02-01 21:36:42 +0300

    update ContactInput.jsx

--- whatsapp-chat/src/components/ContactInput.jsx
+++ whatsapp-chat/src/components/ContactInput.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 const ContactInput = ({ newContact, setNewContact, addContact }) => {
   return (
-    <div>
-      <input 
-        type="text" 
-        placeholder="Номер телефона" 
-        value={newContact} 
-        onChange={(e) => setNewContact(e.target.value)} 
+    <div className="input-contact">
+      <input
+        type="text"
+        placeholder="Номер телефона"
+        value={newContact}
+        onChange={(e) => setNewContact(e.target.value)}
+        className="input-contact-field"
+        required
+        autoFocus
       />
-      <button onClick={addContact}>Добавить чат</button>
+      <button onClick={addContact} className='add-chat-button'>Добавить чат</button>
     </div>
   );
 };
