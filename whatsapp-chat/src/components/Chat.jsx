eacae2905d94efd3bf7572ce785f4d587cc10a99
nnadya <n.d.niki2012@gmail.com>
2025-02-02 22:11:06 +0300

    update Chat.jsx

--- whatsapp-chat/src/components/Chat.jsx
+++ whatsapp-chat/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -22,13 +22,16 @@ const Chat = ({ isLoggedIn, setIsLoggedIn }) => {
   const sendMessage = async () => {
     if (!currentChat) return; // Если нет выбранного чата, не отправляем сообщение
     try {
-      await axios.post(`https://api.green-api.com/waInstance${idInstance}/sendMessage/${apiTokenInstance}`, {
+      await axios.post(
+        `https://api.green-api.com/waInstance${idInstance}/sendMessage/${apiTokenInstance}`,
+        {
           chatId: `${currentChat}@c.us`, // номер аккаунта собеседника
           message: textMessage,
-        }, {
-          headers: {
-          'Content-Type': 'application/json'
         },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
       );
       setMessages(prevMessages => [...prevMessages, { text: textMessage, sender: 'me' }]);
@@ -38,26 +41,43 @@ const Chat = ({ isLoggedIn, setIsLoggedIn }) => {
     }
   };
 
-  const fetchMessages = async () => {
+  const deleteNotification = useCallback(async (receiptId) => {
+    const url = `https://api.green-api.com/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receiptId}`;
+    try {
+      await axios.delete(url);
+    } catch (error) {
+      console.error('Ошибка при удалении уведомления');
+    }
+  }, [idInstance, apiTokenInstance]);
+
+  const fetchMessages = useCallback(async () => {
     if (!currentChat) return; // Не запрашиваем сообщения, если нет текущего чата
     try {
-      const response = await axios.get(`https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`);
+      const response = await axios.get(
+        `https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`
+      );
 
-    // Проверка на наличие новых сообщений
-    //console.log('response.data.body ->', response.data.body);
-      if (response.data && response.data.length > 0) {
-        const newMessages = response.data
-          .filter(msg => msg.sender === currentChat) // Фильтруем сообщения по текущему чату
-          .map(msg => ({
-            text: msg.message,
-            sender: 'other',
-          }));
-        setMessages(prevMessages => [...prevMessages, ...newMessages]);
+      if (response.data) {
+        if (response.data.body.typeWebhook === 'incomingMessageReceived') {
+          const { senderData, messageData } = response.data.body;
+          const newMessage = {
+            text: messageData.textMessageData.textMessage,
+            sender: senderData.sender,
+          };
+          setMessages(prevMessages => [...prevMessages, newMessage]);
+        }
       }
+
+      try {
+        await deleteNotification(response.data.receiptId);
+      } catch(error) {
+        console.error('Ошибка при удалении уведомления', error.message);
+      }
+
     } catch(error) {
-      console.error('Ошибка при получении сообщений:', error);
+      console.error('Ошибка при получении сообщений', error);
     }
-  };
+  }, [idInstance, apiTokenInstance, currentChat, deleteNotification]);
 
   const addContact = () => {
     if (newContact && !contacts.includes(newContact)) {
@@ -74,16 +94,16 @@ const Chat = ({ isLoggedIn, setIsLoggedIn }) => {
 
   useEffect(() => {
     fetchMessages();
-    /* const interval = setInterval(fetchMessages, 5000);
-    return () => clearInterval(interval); */
+    const interval = setInterval(fetchMessages, 5000);
+    return () => clearInterval(interval);
   }, [currentChat, fetchMessages]);
 
   const handleLogout = () => {
     localStorage.removeItem('idInstance');
     localStorage.removeItem('apiTokenInstance');
-    setIsLoggedIn(false); // Устанавливаем состояние неавторизованного пользователя
-    navigate('/login'); // Перенаправляем на страницу логина
-};
+    setIsLoggedIn(false);
+    navigate('/login');
+  };
 
   return (
     <div className="container">
@@ -97,10 +117,7 @@ const Chat = ({ isLoggedIn, setIsLoggedIn }) => {
         {currentChat ? (
           <>
             <h3>Чат с {currentChat}</h3>
-            <div className="message-list">
-              <MessageList messages={messages} />
-            </div>
-            
+            <MessageList messages={messages} />
             <MessageInput textMessage={textMessage} setTextMessage={setTextMessage} sendMessage={sendMessage} />
           </>
         ) : (
